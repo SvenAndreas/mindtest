912a67aa3939f5782e22bc99990763bb799b8cf4
Sven Andreas Clausz <svenandreasclausz@gmail.com>
2024-12-18 11:07:58 -0300

    FIX: Modify useLocalStorage

--- src/app/Shared/hooks/useLocalStorage.ts
+++ src/app/Shared/hooks/useLocalStorage.ts
@@ -1,33 +1,18 @@
-import { useEffect, useState } from "react";
+type ValidKeys = 'tasks'
 
-type ValidKeys = 'tasks';
-
-export const useLocalStorage = (key: ValidKeys) => {
-  const [isClient, setIsClient] = useState(false);
-
-  useEffect(() => {
-    setIsClient(true);
-  }, []);
-
-  const setItem = (value: unknown) => {
-    if (isClient) {
-      window.localStorage.setItem(key, JSON.stringify(value));
+export const useLocalStorage = (key:ValidKeys)=>{
+    const setItem = (value:unknown)=>{
+        localStorage.setItem(key,JSON.stringify(value))
     }
-  };
 
-  const getItem = () => {
-    if (isClient) {
-      const item = window.localStorage.getItem(key);
-      return item ? JSON.parse(item) : null;
+    const getItem = ()=>{
+        const item = localStorage.getItem(key)
+        return item ? JSON.parse(item) : null
     }
-    return null;
-  };
 
-  const removeItem = () => {
-    if (isClient) {
-      window.localStorage.removeItem(key);
+    const removeItem = ()=>{
+        localStorage.removeItem(key)
     }
-  };
 
-  return { setItem, getItem, removeItem };
-};
+    return {setItem,getItem,removeItem}
+}
