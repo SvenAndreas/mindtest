1f783d584528904bde4244b5f5c008e6cdf43e45
Sven Andreas Clausz <svenandreasclausz@gmail.com>
2024-12-18 08:46:16 -0300

    FIX: window is not defined in useLocalStorageHook

--- src/app/Shared/hooks/useLocalStorage.ts
+++ src/app/Shared/hooks/useLocalStorage.ts
@@ -1,18 +1,33 @@
-type ValidKeys = 'tasks'
+import { useEffect, useState } from "react";
 
-export const useLocalStorage = (key:ValidKeys)=>{
-    const setItem = (value:unknown)=>{
-        window.localStorage.setItem(key,JSON.stringify(value))
+type ValidKeys = 'tasks';
+
+export const useLocalStorage = (key: ValidKeys) => {
+  const [isClient, setIsClient] = useState(false);
+
+  useEffect(() => {
+    setIsClient(true);
+  }, []);
+
+  const setItem = (value: unknown) => {
+    if (isClient) {
+      window.localStorage.setItem(key, JSON.stringify(value));
     }
+  };
 
-    const getItem = ()=>{
-        const item = window.localStorage.getItem(key)
-        return item ? JSON.parse(item) : null
+  const getItem = () => {
+    if (isClient) {
+      const item = window.localStorage.getItem(key);
+      return item ? JSON.parse(item) : null;
     }
+    return null;
+  };
 
-    const removeItem = ()=>{
-        window.localStorage.removeItem(key)
+  const removeItem = () => {
+    if (isClient) {
+      window.localStorage.removeItem(key);
     }
+  };
 
-    return {setItem,getItem,removeItem}
-}
+  return { setItem, getItem, removeItem };
+};
