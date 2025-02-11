0870042ee89e26d5aa7cd1b1d720213e8bacc092
Sven Andreas Clausz <svenandreasclausz@gmail.com>
2025-02-11 17:50:07 -0300

    Update NavbarLogo.tsx

--- src/app/Shared/components/Navbar/NavbarLogo.tsx
+++ src/app/Shared/components/Navbar/NavbarLogo.tsx
@@ -3,7 +3,7 @@ import React from "react";
 // import Logo from '../../assets/mindfactory-logo.webp'
 function NavbarLogo() {
   return (
-    <div className="cursor-default rounded-md flex flex-col gap-4 bg-white shadow-xl px-3">
+    <div className="cursor-default rounded-md flex flex-col gap-1 bg-white shadow-xl px-3">
 {/*       <Image loading="eager" width={200} src={Logo} alt="Logo app" /> */}
 <p className="font-bold">Tareas app</p>
 {/*       <p className="text-center relative -top-4 -left-3">Organizá tu día</p> */}
